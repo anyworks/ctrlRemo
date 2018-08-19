8bc08ac98f75704331f525cbef64d33a16413d80
doomo3 <anyworks.onm@gmail.com>
2018-08-19 20:29:46 +0900

    IR is supported

--- index.js
+++ index.js
@@ -4,79 +4,150 @@ var request = require( 'request' );
 var rp = require( 'request-promise' );
 var fs = require( 'fs' );
 var encode = require( 'hashcode' ).hashCode;
-var cachedRequest = require('cached-request')(request)
-var cacheDirectory = "/tmp/cache";
+var cachedRequest = require('cached-request')(request) //reserve
+var cacheDirectory = "/tmp/cache"; //reserve
 cachedRequest.setCacheDirectory(cacheDirectory);
+
 var headers = {
   "Authorization" : `Bearer ${process.env.ACTK_REMO}`
   ,"Content-Type" : "application/json"
 };
+//for debugging
 var ps/*?*/ = process.argv[1];
 console.log(process.cwd());
+
+//global settings
 var version = "1";
 var baseURL="api.nature.global";
-var response;
 
+exports.generateIRList = function(event, context, callback) {
+  this.get({ url : "https://api.nature.global/1/appliances"},{},(err,res)=>{
+
+    var contents = res.filter((e)=>{return e.type === "IR"}).reduce((prev,c)=>{
+      prev[c.nickname] = c.signals.reduce((p,c)=>{p[c.name]=c.id;return p;},{});
+      return prev;
+    },{});
+
+    var dt = JSON.stringify(contents,null,"\t");
+    console.log(dt);
+    fs.writeFile(`./ir.json`, dt, 'utf8');
+    callback(null,dt);
+  });
+
+}
+//This function is for switching to dummy a behavior of requesting to actual server.
 function nrp(){
   return new Promise((resolve,reject)=>{
     resolve({done:"dummy"});
   });
 }
 
-function writeContent(you,hash,content){
-  var dt = content || `{empty : ${you}`;
-  fs.writeFile(`./cache/${hash}.json`, dt, 'utf8');
+function writeContent(you,name,content){
+  var dt = content || `{empty : ${you}}`;
+  fs.writeFile(`./cache/${name}.json`, dt, 'utf8');
 }
 
-exports.get = function(event, context, callback) {
-  var vapi = `${version}/${event.api}`;
-  var url = `https://${baseURL}/${vapi}`;
-  var hash = encode().value(url);
-  if (!fs.existsSync(`./cache/${hash}.json`)) {
-    nrp({
-      url : url,
-      method: "get",
-      headers : headers,
-      json: true
-    }).then((res)=>{
-      if (!fs.existsSync("./cache")) {
-      var content = JSON.stringify(res,null,"\t");
-        fs.mkdirSync("./cache", "0744");
-      }
-      callback(null,res);
-      writeContent(content);
-      return;
-    });
-  }else{
-    var cache =  fs.readFileSync(`./cache/${hash}.json`, 'utf8');
-    callback(null,JSON.parse(cache));
+var urlfactroy= function(type,id,order)
+{
+  var base = `https://${baseURL}/`;
+  var fmt = {
+    url : "",
+    data : ""
+  };
+
+  if(type === "IR"){
+    fmt.url = `1/signals/${id}/send`;
   }
+
+  if(type === "AC"){
+    fmt.url = `appliances/${id}/aircon_settings`;
+//    fmt.data = (order===false) ? "power-off" : "power-on";
+  }
+  fmt.url = base + fmt.url;
+  return fmt;
+};
+//C:\Users\alice\.atom\packages\atom-wallaby\projects\77cea3325a3daa31\instrumented
+exports.delayed = function(event, context, callback) {
+  var irs = fs.readFileSync("./ir.json","utf-8");
+  irs = JSON.parse(irs);
+  console.log(irs);
+  var promises = [];
+  event.orders.forEach((e,idx)=>{
+    var id = irs[e.order[0]][e.order[1]];
+
+    var orderObj = urlfactroy("IR",id);
+    console.log(e.delay);
+    console.log(orderObj);
+    setTimeout((i)=>{
+      promises.push(postImpl(orderObj.url));
+      if(event.orders.length == (i+1)){
+        Promise.all(promises).then((res)=>{
+            console.log("sending ok");
+            callback(null,res);
+        });
+      }
+    },e.delay*(idx+1),idx);
+  });
+};
+
+exports.get = function(event, context, callback) {
+  getImpl(event.url).then((res)=>{callback(null,res)});
+}
+
+var getImpl = function(url,data) {
+  console.log(url);
+  return new Promise((resolve,reject)=>{
+    var hash = encode().value(url);
+    console.log(hash);
+    if (!fs.existsSync(`./cache/${hash}.json`)) {
+      nrp({   //### need to change### Default behavior is as dummy mode(using cache file).
+        url : url,
+        method: "get",
+        headers : headers,
+        json: true
+      }).then((res)=>{
+        if (!fs.existsSync("./cache")) {
+          var content = JSON.stringify(res,null,"\t");
+          fs.mkdirSync("./cache", "0744");
+        }
+
+        console.log(content);
+        writeContent("get",hash,content);
+        resolve(res);
+      });
+    }else{
+      var cache =  fs.readFileSync(`./cache/${hash}.json`, 'utf8');
+      resolve(JSON.parse(cache));
+    }
+  });
+
 };
 
 exports.post = function(event, context, callback) {
-  var vapi = `${version}/${event.api}`;
-  var url = `https://${baseURL}/${vapi}`;
-  var hash = encode().value(url);
-
-  if (!fs.existsSync(`./cache/${hash}.json`)) {
-    nrp({
-      url : url,
-      method: "post",
-      headers : headers,
-      json: true,
-      form : event.params
-    }).then((res)=>{
-      if (!fs.existsSync("./cache")) {
-        fs.mkdirSync("./cache", "0744");
-      }
-      var content = JSON.stringify(res,null,"\t");
-      writeContent(content);
-      callback(null,res);
-      return;
-    });
-  }else{
-    var cache =  fs.readFileSync(`./cache/${hash}.json`, 'utf8');
-    callback(null,JSON.parse(cache));
-  }
-
+  postImpl(event.url,event.data).then((res)=>{
+    callback(null,res);
+  }).catch((err)=>{
+    callback(null,err)
+  });
 };
+
+function postImpl(url,data){
+  console.log("############# sending ###################");
+  console.log(url);
+  console.log(data);
+  return new Promise((resolve,reject)=>{
+    var hash = encode().value(url);
+      nrp({     //### need to change### Default behavior is as dummy mode(using cache file).
+        url : url,
+        method: "post",
+        headers : headers,
+        json: true,
+        form : data
+      }).then((res)=>{
+        resolve(res);
+      }).catch((err)=>{
+        reject(err);
+      });
+  });
+
+}
---
+++ tests/remoteAPISpec.js
@@ -0,0 +1,36 @@
+'use strict';
+var index = require('../index.js');
+var fs = require( 'fs' );
+
+describe('appliances', function() {
+  beforeEach(function() {
+    var originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+  });
+
+  it('generate IR list', function(done) {
+    index.generateIRList({},{},done);
+  });
+
+  xit('delayed request', function(done) {
+    index.delayed({
+      orders : [
+      {
+        delay : 3000,
+        order : ["電気","オン"]
+      },
+      {
+        delay : 3000,
+        order : ["電気","暗く"]
+      },
+      {
+        delay : 3000,
+        order : ["電気","オフ"]
+      }
+    ]
+    },{},(res)=>{
+       done(res);
+    });
+  });
+
+});
